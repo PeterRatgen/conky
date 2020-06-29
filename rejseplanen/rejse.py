92bd634099161cbf4bc8e65d6c94d6aa0079cdd6
Peter Heilbo Ratgen <peter@pratgen.dk>
2020-06-30 00:23:21 +0200

    added comments

--- rejseplanen/rejse.py
+++ rejseplanen/rejse.py
@@ -12,9 +12,11 @@ baseurl = 'http://xmlopen.rejseplanen.dk/bin/rest.exe/'
 departureboardurl = 'departureBoard?id=461077600'
 formaturl = '&format=json'
 
+# Retrieve the departurebord from rejseplanen
 jsonurl = requests.get(baseurl+departureboardurl+formaturl).json()
 departure_list = jsonurl['DepartureBoard']['Departure']
 
+# Get the reference uri from the top entry on the departureboard
 journey_detail = requests.get(departure_list[0]['JourneyDetailRef']['ref']).json()
 
 try:
@@ -26,6 +28,7 @@ try:
 
   cursor = connection.cursor()
 
+  # Insert the data for each entry of the departureboard
   for i in departure_list:
     if "rtTime" in i.keys():
       insert_query = "INSERT INTO departures (transport_id, date, time, name, stop, direction, journey_detail_ref, rtTime, rtDate) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
@@ -37,10 +40,12 @@ try:
       cursor.execute(insert_query, insertion_record)
   connection.commit()
 
+  # get id of top departure
   select_latest = "select (id) from (select (id) from departures order by id desc limit 20) as foo order by id asc limit 1;"
   cursor.execute(select_latest)
   lastest_id = cursor.fetchone()[0]
 
+  # insert journey into journey_table with the id of top departure
   insert_journey_query = "INSERT INTO journey_table (departure_id, query) VALUES (%s, %s)"
   insertion_record = (lastest_id, json.dumps(journey_detail))
   cursor.execute(insert_journey_query, insertion_record)
